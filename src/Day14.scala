71528d8e79897d8c171e5ac6276c1cb9a2930d8d
Margaret Pearce <margaret.pearce@gmail.com>
2016-12-15 11:09:50 -0500

    Day 14 part B

--- src/Day14.scala
+++ src/Day14.scala
@@ -4,28 +4,41 @@ import javax.xml.bind.DatatypeConverter.printHexBinary
 import scala.collection.mutable
 
 class Day14(salt: String) {
+  // Part A
   private val hashes = mutable.Map[Int, String]()
   private val keys = mutable.ArrayBuffer[String]()
   private val keyIndex = mutable.ArrayBuffer[Int]()
 
+  // Part B
+  private val stretchedHashes = mutable.Map[Int, String]()
+  private val keysStretched = mutable.ArrayBuffer[String]()
+  private val keyStretchedIndex = mutable.ArrayBuffer[Int]()
+
   def findHashes(): Unit = {
     var i = 0
-    while (keys.length < 64) {
+    while (this.keys.length < 64 || this.keysStretched.length < 64) {
       // Make sure the ith salt has been generated and saved
       this.generateAndSaveHash(i)
+      this.generateAndSaveStretchedHash(i)
 
       // Check if this is a key
-      if (this.hashIsKey(i)) {
-        keys.append(this.hashes(i))
-        keyIndex.append(i)
-        println("Found key #" + i + ": " + this.hashes(i))
+      if (keys.length < 64 && this.hashIsKey(i)) {
+        this.keys.append(this.hashes(i))
+        this.keyIndex.append(i)
+      }
+
+      // Check if the stretched hash is a key (part B)
+      if (this.keysStretched.length < 64 && this.stretchedHashIsKey(i)) {
+        this.keysStretched.append(this.stretchedHashes(i))
+        this.keyStretchedIndex.append(i)
       }
 
       // Continue looking for keys
       i = i + 1
     }
 
-    println("Index " + (i-1) + " generated the 64th hash")
+    println("Index " + this.keyIndex(63) + " generated the 64th hash (part A)")
+    println("Index " + this.keyStretchedIndex(63) + " generated the 64th hash (part B)")
   }
 
   def generateAndSaveHash(i: Int): String = {
@@ -45,7 +58,7 @@ class Day14(salt: String) {
 
   def md5Hex(s: String): String = {
     // Get MD5 hash as a string of lowercase hex digits
-    printHexBinary(MessageDigest.getInstance("MD5").digest(s.getBytes))
+    printHexBinary(MessageDigest.getInstance("MD5").digest(s.getBytes)).toLowerCase
   }
 
   def hashIsKey(hashIndex: Int): Boolean = {
@@ -70,7 +83,51 @@ class Day14(salt: String) {
         return true
       }
     }
-    return false
+    false
+  }
+
+  def generateAndSaveStretchedHash(i: Int): String = {
+    if (!this.stretchedHashes.contains(i)) {
+      // Get the original hash
+      var hash = this.generateAndSaveHash(i)
+
+      // Rehash this hash 2016 times to get the stretched hash
+      for (stretchNum <- 1 to 2016) {
+        val nextHashInput = hash.mkString
+        val nextHash = this.md5Hex(nextHashInput)
+        hash = nextHash
+      }
+
+      // Add to the collection of stretched hashes
+      this.stretchedHashes.put(i, hash)
+    }
+
+    this.stretchedHashes(i)
+  }
+
+  def stretchedHashIsKey(hashIndex: Int): Boolean = {
+    val hash = this.stretchedHashes(hashIndex)
+
+    // Are there 3 repeating characters in this hash?
+    for (i <- 0 to hash.length - 3) {
+      if (hash.charAt(i) == hash.charAt(i+1) && hash.charAt(i+1) == hash.charAt(i+2)) {
+        // If one is found, then check the next 1000 hashes to see if 5 in a row are found
+        return this.nextThousandStretchedHashesContainFiveInARow(hashIndex+1, hash.charAt(i).toString * 5)
+      }
+    }
+    // If no repeats of 3 are ever found, return false
+    false
+  }
+
+  def nextThousandStretchedHashesContainFiveInARow(startingIndex: Int, fiveRepeating: String): Boolean = {
+    // Loop through the next 1000 hashes to see if five of the same character appear in a row
+    for (i <- startingIndex to startingIndex+1000) {
+      val hash = this.generateAndSaveStretchedHash(i)
+      if (hash.contains(fiveRepeating)) {
+        return true
+      }
+    }
+    false
   }
 }
 
