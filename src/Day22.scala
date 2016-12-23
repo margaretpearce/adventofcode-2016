fce26f22bee8f049618b497a3ae0c19eed8d83c3
Margaret Pearce <margaret.pearce@gmail.com>
2016-12-22 23:21:47 -0500

    Day 22, parsing input only

---
+++ src/Day22.scala
@@ -0,0 +1,39 @@
+import scala.collection.mutable.HashMap
+
+class Day22 {
+  private var usedSpace = HashMap[(Int,Int), Int]()
+  private var totalSpace = HashMap[(Int,Int), Int]()
+
+  def setNodeInformation(fileName: String): Unit = {
+    val nodes = scala.io.Source.fromFile(fileName)
+
+    for (node <- nodes.getLines()) {
+      if (node.startsWith("/dev/grid/node")) {
+        // Get x, y, size, and used
+        val nodeInfo = node.split(" +")
+
+        val xyInfo = nodeInfo(0).split("-")
+        val x = Integer.parseInt(xyInfo(1).substring(1), 10)
+        val y = Integer.parseInt(xyInfo(2).substring(1), 10)
+
+        val size = Integer.parseInt(nodeInfo(1).trim().split("T")(0), 10)
+        val used = Integer.parseInt(nodeInfo(2).trim().split("T")(0), 10)
+
+        this.usedSpace.put((x,y), used)
+        this.totalSpace.put((x,y), size)
+      }
+    }
+  }
+
+  def getNumberOfNodes(): Int = {
+    this.totalSpace.size
+  }
+}
+
+object Day22Puzzle {
+  def main(args: Array[String]): Unit = {
+    val puzzle = new Day22()
+    puzzle.setNodeInformation(args(0))
+    println("# nodes: " + puzzle.getNumberOfNodes())
+  }
+}
