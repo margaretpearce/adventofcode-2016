a88eae951f2ac9ccd170269edcbb0ad6577a4454
Margaret Pearce <margaret.pearce@gmail.com>
2016-12-27 21:52:50 -0500

    Day 22 part b

--- src/Day22.scala
+++ src/Day22.scala
@@ -1,32 +1,15 @@
 import scala.collection.mutable
-import scala.collection.mutable.HashMap
-
-class State(x:Int, y:Int, costVal:Int, cluster:Array[Array[Int]]) {
-  private var cost = costVal
-
-  def getHeuristicCost(): Int = {
-    x + y
-  }
-
-  def getCost(): Int = {
-    cost
-  }
-
-  def setCost(newCost:Int): Unit = {
-    cost = newCost
-  }
-}
 
 class Day22 {
-  private var usedSpace = HashMap[(Int,Int), Int]()
-  private var totalSpace = HashMap[(Int,Int), Int]()
+  private val usedSpace = mutable.HashMap[(Int,Int), Int]()
+  private val totalSpace = mutable.HashMap[(Int,Int), Int]()
   private var x_max = -1
   private var y_max = -1
 
   def setNodeInformation(fileName: String): Unit = {
     val nodes = scala.io.Source.fromFile(fileName)
 
-    for (node <- nodes.getLines()) {
+      for (node <- nodes.getLines()) {
       if (node.startsWith("/dev/grid/node")) {
         // Get x, y, size, and used
         val nodeInfo = node.split(" +")
@@ -48,7 +31,7 @@ class Day22 {
     }
   }
 
-  def getNumberOfNodes(): Int = {
+  def getNumberOfNodes: Int = {
     this.totalSpace.size
   }
 
@@ -66,100 +49,83 @@ class Day22 {
     numPairs
   }
 
-  def getArrayState(): Array[Array[Int]] = {
-    val array = Array.ofDim[Int](this.x_max, this.y_max)
-    for (x <- 0 to this.x_max) {
-      for (y <- 0 to this.y_max) {
-        array(x)(y) = this.usedSpace(x,y)
-      }
-    }
-    array
-  }
+  def printGrid = {
+    val nodeToMove = (this.x_max, 0)
+    val emptyNode = this.findEmptyNode()
+    val largeNodeCutoffSize = this.totalSpace(emptyNode)
 
-  def stateIsGoal(state: Array[Array[Int]]): Boolean = {
-    state.map(i => i.sum).sum == state(0)(0)
-  }
+    for (y <- 0 to this.y_max) {
+      for (x <- 0 to this.x_max) {
+        val used = this.usedSpace((x,y))
 
-  def getStateHeuristic(state: Array[Array[Int]]): Int = {
-    state.map(i => i.count(p => p > 0)).sum
-  }
-
-  def getNextStates(state: Array[Array[Int]]): Array[Array[Array[Int]]] = {
-    null
-  }
-
-  def searchAStar: Int = {
-    // Set of nodes already evaluated
-    val closedSet = mutable.ArrayBuffer[Array[Array[Int]]]()
-
-    // Keep states in priority queue with the smallest estimated costs appearing first
-    implicit def orderedState(f: (Int, Array[Array[Int]])): Ordered[(Int, Array[Array[Int]])] =
-      new Ordered[(Int, Array[Array[Int]])] {
-        def compare(other: (Int, Array[Array[Int]])) = -1 * f._1.compare(other._1)
-      }
-
-    // Set of currently discovered states still to be evaluated
-    val openSet = mutable.PriorityQueue.empty[(Int, Array[Array[Int]])]
-    val initialState = this.getArrayState()
-    val initialHeuristic = this.getStateHeuristic(initialState)
-    openSet.enqueue((initialHeuristic, initialState))
-
-    // Previous steps to the current "best" state
-    val cameFrom = mutable.Map[Array[Array[Int]], Array[Array[Int]]]()
-
-    // Costs of getting to each node (known lowest)
-    val currentCost = mutable.HashMap[Array[Array[Int]], Int]()
-    currentCost.put(initialState, 0)
-
-    // Heuristic cost of getting from this node to the goal state
-    val estimatedCost = mutable.HashMap[Array[Array[Int]], Int]()
-    estimatedCost.put(initialState, initialHeuristic)
-
-    // Search loop
-    while (openSet.nonEmpty) {
-      // Get the state with the lowest estimated score
-      val stateTuple = openSet.dequeue()
-      val state = stateTuple._2
-      val cost = stateTuple._1
-
-      if (this.stateIsGoal(state)) {
-//        this.endState = state.copyState()
-//        this.printPath(cameFrom)
-//        openSet.clear()
-        return cost
-      }
-
-      closedSet.append(state)
-      val nextStates = this.getNextStates(state)
-
-      for (nextState <- nextStates) {
-        if (!closedSet.contains(nextState)) {
-          val newScore = cost + 1
-
-          if (!currentCost.contains(nextState) ||
-            (currentCost.contains(nextState) && newScore < currentCost(nextState))) {
-            currentCost.put(nextState, newScore)
-            cameFrom.put(nextState, state)
-            estimatedCost.put(nextState, this.getStateHeuristic(nextState))
-
-//            nextState.setCurrentScore(newScoreEstimate)
-//            cameFrom.put(nextState, state)
-//            nextState.setEstimatedScore(nextState.getCurrentScore() + nextState.estimateHeuristic())
-          }
-
-          if (openSet.filter(p => p._2.equals(nextState)).isEmpty) {
-            openSet.enqueue((estimatedCost(nextState), nextState))
-          }
-
-//          if (!openSet.contains(nextState)) {
-//            openSet.enqueue((nextState.getEstimatedScore(), nextState))
-//          }
+        if (x == 0 && y == 0) {
+          print("X")
+        }
+        else if ((x,y) == nodeToMove) {
+          print("G")
+        }
+        else if ((x,y) == emptyNode) {
+          print("_")
+        }
+        else if (used <= largeNodeCutoffSize) {
+          print(".")
+        }
+        else {
+          print("#")
         }
       }
+      print("\n")
+    }
+  }
+
+  def findEmptyNode(): (Int,Int) = {
+    val nodes = this.usedSpace.keySet.toList
+
+    for (node <- nodes) {
+      if (this.usedSpace(node) == 0) {
+        return node
+      }
     }
 
-    // Failure case
-    -1
+    (-1,-1)
+  }
+
+  def findWall(): List[(Int,Int)] = {
+    val wallList = mutable.ListBuffer[(Int,Int)]()
+    val emptyNode = this.findEmptyNode()
+
+    for (y <- 0 to this.y_max) {
+      for (x <- 0 to this.x_max) {
+        val used = this.usedSpace((x,y))
+        if (used > this.totalSpace(emptyNode)) wallList.append((x,y))
+      }
+    }
+
+    wallList.toList
+  }
+
+  def moveTopRightToTopLeft(): Int = {
+    // Empty node that can be used to swap data
+    val emptyNode = this.findEmptyNode()
+
+    // Wall nodes that we need to move around
+    val wallNodes = this.findWall()
+    val wallXMin = wallNodes.map(p => p._1).min
+    val wallYMax = wallNodes.map(p => p._2).max
+
+    // Move the empty node to the row below the wall
+    val moveUpToWall = emptyNode._2 - wallYMax - 1
+
+    // Move to the left wall node and then over one more so we can move up
+    val moveLeftOfWall = emptyNode._1 - wallXMin + 1
+
+    // Move up to the wall, then up to the top, then right to the goal
+    val moveToTarget = 1 + wallYMax + (this.x_max - wallXMin + 1)
+
+    // To move the target to the left, need to do a sequence of 5 moves around the target
+    val moveTargetToGoal = 5*(this.x_max-1)
+
+    moveUpToWall + moveLeftOfWall + moveToTarget + moveTargetToGoal
   }
 }
 
@@ -167,7 +133,14 @@ object Day22Puzzle {
   def main(args: Array[String]): Unit = {
     val puzzle = new Day22()
     puzzle.setNodeInformation(args(0))
-    println("# nodes: " + puzzle.getNumberOfNodes())
+
+    // Part A
+    println("# nodes: " + puzzle.getNumberOfNodes)
     println("# viable pairs: " + puzzle.countViablePairs())
+
+    // Part B
+    println("# moves target to goal: " + puzzle.moveTopRightToTopLeft())
+    println("\nGrid:")
+    println(puzzle.printGrid)
   }
 }
